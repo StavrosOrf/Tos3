3482bc9e01b8f2a4000765f11384fec35e546973
lariorAT <www.leuthor@yahoo.gr>
2017-10-30 22:02:20 +0200

    test n1

--- bios_example1.c
+++ bios_example1.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 
 void bootfunc() {
+  fprintf(stderr,"hello from bios123 \n");
   fprintf(stderr, "Hello from core %u\n", cpu_core_id);
 }
 
 int main()
 {
+  
   vm_boot(bootfunc, 4, 0);
   return 0;
 }
